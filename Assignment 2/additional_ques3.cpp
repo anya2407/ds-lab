13e88ddc328aed1fb471e53a839076a761488455
anya2407 <anyaarora2407@gmail.com>
2025-09-05 14:35:01 +0530

    additional_ques3.cpp

--- Assignment 2/additional_ques3.cpp
+++ Assignment 2/additional_ques3.cpp
@@ -1,24 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-bool isAnagram(string s1, string s2) {
+bool isanagram(string s1, string s2) {
 
     if (s1.size() != s2.size())
-    { return false;}
-    sort(s1.begin(), s1.end());
+    {return false;}
+    sort (s1.begin(), s1.end());
     sort(s2.begin(), s2.end());
     if (s1==s2){
-        return true;
-    }
+        return true;}
     else
     {
-        return false;
-    }
-    
+      return false;
+}
 }
 
 int main() {
     string s1 = "listen", s2 = "silent";
-    cout<<(isAnagram(s1, s2) ? "YES" : "NO");
-
+    cout<<(isanagram(s1, s2) ? "yes" : "no");
 }
+
